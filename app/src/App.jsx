d4edc6d173b19c932eaeb56abfed7eb53688ef78
ibraim <ikoraikir@gmail.com>
2022-12-04 05:51:23 -0800

    navbar

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,7 +1,7 @@
 import {  children } from "react";
 import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
-import './App.css'
+
 
 import Home from './pages/Home/Home'
 import Product from './pages/Product/Product'
--- app/src/components/Navbar.jsx
+++ app/src/components/Navbar.jsx
@@ -1,8 +1,63 @@
-import React from 'react'
+import React, { useState } from 'react'
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import SearchIcon from "@mui/icons-material/Search";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import FavoriteBorderOutlinedIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
+      const [open,setOpen] = useState(false)
+  
   return (
-    <div>Navbar</div>
+    <div className="navbar  h-[80px]  ">
+      <div className=" flex items-center justify-between py-[10px] px-[30px] ">
+        <div className=" flex items-center gap-6 ">
+          <div className="item ">
+            <img src="/img/en.png" alt="" />
+            <KeyboardArrowDownIcon />
+          </div>
+          <div className="item">
+            <span>USD</span>
+            <KeyboardArrowDownIcon />
+          </div>
+          <div className="item">
+            <Link className ="link" to="/products/1">Women</Link>
+          </div>
+          <div className="item">
+            <Link className ="link" to="/products/2">Men</Link>
+          </div>
+          <div className="item">
+            <Link className ="link" to="/products/3">Children</Link>
+          </div>
+        </div>
+        <div className=" text-base ">
+          <Link className ="link" to="/">LAMASTORE</Link>
+        </div>
+        <div className="right flex items-center gap-6">
+          
+          <div className="item">
+            <Link className ="link" to="/">About</Link>
+          </div>
+          <div className="item">
+            <Link className ="link" to="/">Contact</Link>
+          </div>
+          <div className="item">
+            <Link className ="link" to="/">Stores</Link>
+          </div>
+          <div className=" flex items-center gap-4 cursor-pointer text-red-500">
+            <SearchIcon/>
+            <PersonOutlineOutlinedIcon/>
+            <FavoriteBorderOutlinedIcon/>
+            <div className=" relative" onClick={()=>setOpen(!open)}>
+              <ShoppingCartOutlinedIcon/>
+              <span className='text-xs absolute w-5 h-5 bg-red-600 rounded-full text-white flex items-center justify-center right-[-10px] top-[-10px]'>1</span>
+            </div>
+          </div>
+        </div>
+      </div>
+      {open && <h1>h</h1>}
+    </div>
   )
 }
 
