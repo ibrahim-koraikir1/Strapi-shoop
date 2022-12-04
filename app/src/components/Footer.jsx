2c8d4d27857547a3a034b185de5ea5520b326402
ibraim <ikoraikir@gmail.com>
2022-12-04 06:44:00 -0800

    footer

--- app/src/components/Footer.jsx
+++ app/src/components/Footer.jsx
@@ -2,6 +2,54 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+      <div className=" m-[100px 200px 20px 200px]">
+      <div className=" flex gap-12">
+        <div className="iten">
+          <h1>Categories</h1>
+          <span>Women</span>
+          <span>Men</span>
+          <span>Shoes</span>
+          <span>Accessories</span>
+          <span>New Arrivals</span>
+        </div>
+        <div className="iten">
+          <h1>Links</h1>
+          <span>FAQ</span>
+          <span>Pages</span>
+          <span>Stores</span>
+          <span>Compare</span>
+          <span>Cookies</span>
+        </div>
+        <div className="iten">
+          <h1>About</h1>
+          <span>
+            Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore. Lorem ipsum dolor sit
+            amet conse ctetur adipisicing elit, seddo eiusmod tempor incididunt
+            ut labore etdolore.
+          </span>
+        </div>
+        <div className="iten">
+          <h1>Contact</h1>
+          <span>
+            Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore. Lorem ipsum dolor sit
+            amet conse ctetur adipisicing elit, seddo eiusmod tempor incididunt
+            ut labore etdolore.
+          </span>
+        </div>
+      </div>
+      <div className=" flex items-center justify-between mt-12">
+        <div className="flex items-center ">
+          <span className=" text-[#2879fe] text-[24px] font-bold">Lamastore</span>
+          <span className="ml-5 text-xs text-gray-600">
+            © Copyright 2023. All Rights Reserved
+          </span>
+        </div>
+        <div className="right">
+          <img  className='h-12' src="/img/payment.png" alt="" />
+        </div>
+      </div>
+    </div>
   )
 }
