9c0841e6850d4d5aa365e41f677d181f16c4e2d0
ibraim <ikoraikir@gmail.com>
2022-12-09 09:09:33 -0800

    card

--- app/src/components/Card.jsx
+++ app/src/components/Card.jsx
@@ -2,6 +2,18 @@ import React from 'react'
 import { Link } from "react-router-dom";
 export default function Card({ item }) {
   return (
-    <div>{ item }</div>
+    <Link className="link" to={`/product/${item.id}`}>
+      <div >
+        <div >
+          {item?.attributes?.isNew && <span>New Season</span>}
+         
+      </div>
+        <h2>{item?.attributes?.title}</h2>
+        <div className="prices">
+          <h3>${item?.oldPrice || item?.attributes?.price + 20}</h3>
+          <h3>${item?.attributes?.price}</h3>
+        </div>
+      </div>
+    </Link>
   )
 }
